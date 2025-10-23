8016f2b6f359324daab74292bf43a2a0391a4e25
Novemas Heka Alfarizi <vemasgamer2@gmail.com>
2025-10-23 10:51:51 +0700

    ndatau

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ let header = document.getElementById("nav");
 let judulHeader = document.getElementById("brand1");
 let judulHeader2 = document.getElementById("brand2");
 let logoBrand = document.getElementById("logo_brand")
-let nav = document.getElementsByClassName("link-nav");
+let navigasi = document.getElementsByClassName("link-nav");
 let main = document.getElementsByClassName("ketentuan_peserta")
+let navMenu = document.getElementsByClassName("penjelasan-header");
+
+navigasi.addEventListener("click", () => {
+    
+})
 
 window.onscroll = function(){Headers()}
 function Headers(){
@@ -11,6 +16,7 @@ function Headers(){
     if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         header.style.position = "fixed";  
         main[0].style.top = "100px"
+        navMenu[0].style.display = "none";
     } else {
         header.style.position = "relative";
         main[0].style.top = "0px"
@@ -18,3 +24,5 @@ function Headers(){
 
 
 }
+
+
